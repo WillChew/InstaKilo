2608000933fc53584faceb1abf5cbdc55424ac08
WillChew <williamchew95@gmail.com>
2018-06-20 19:53:12 -0400

    added segmented views

--- InstaKilo/ViewController.m
+++ InstaKilo/ViewController.m
@@ -15,8 +15,8 @@
 @property (nonatomic,strong)NSArray<NSArray*> *imagesArray;
 @property (weak, nonatomic) IBOutlet UICollectionView *imagesCollection;
 @property (nonatomic, strong)NSArray *nameOfHeaders;
-@property (nonatomic,strong) NSArray<UIImage*> *skyArray;
-@property (nonatomic,strong) NSArray<UIImage*> *otherArray;
+@property (nonatomic,strong) NSArray *skyArray;
+@property (nonatomic,strong) NSArray *otherArray;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControlOutlet;
 
 @end
@@ -38,12 +38,12 @@
     self.skyArray = skyArray;
     
     NSArray *everythingElseArray = @[[[Pictures alloc]initWithImage:[UIImage imageNamed:@"1"] name:@"1"],
-    [[Pictures alloc]initWithImage:[UIImage imageNamed:@"2"] name:@"2"],
-    [[Pictures alloc]initWithImage:[UIImage imageNamed:@"3"] name:@"3"],
-    [[Pictures alloc]initWithImage:[UIImage imageNamed:@"4"] name:@"4"],
-    [[Pictures alloc]initWithImage:[UIImage imageNamed:@"5"] name:@"5"],
-    [[Pictures alloc]initWithImage:[UIImage imageNamed:@"6"] name:@"6"]];
-   
+                                     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"2"] name:@"2"],
+                                     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"3"] name:@"3"],
+                                     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"4"] name:@"4"],
+                                     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"5"] name:@"5"],
+                                     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"6"] name:@"6"]];
+    
     self.otherArray = everythingElseArray;
     
     self.imagesArray = @[skyArray, everythingElseArray];
@@ -57,7 +57,7 @@
 }
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-
+    
     
     if ([self.segmentedControlOutlet selectedSegmentIndex]==0){
         return 1;
@@ -65,14 +65,14 @@
     if ([self.segmentedControlOutlet selectedSegmentIndex] == 1){
         return 1;
     } else {
-    return [self.imagesArray count];
+        return [self.imagesArray count];
     }
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     if ([self.segmentedControlOutlet selectedSegmentIndex] == 0){
         return [self.skyArray count];
-}
+    }
     if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
         return [self.otherArray count];
     }
@@ -80,31 +80,32 @@
     else {
         return [[self.imagesArray objectAtIndex:section] count];
     }
-    }
-        
-        
+}
+
+
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     ImageCellCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ImageCell" forIndexPath:indexPath];
-        
-        
-        
-//        cell.imageView.image = self.imagesArray[indexPath.section][indexPath.row];
+    
+    
+    
+    //        cell.imageView.image = self.imagesArray[indexPath.section][indexPath.row];
     if ([self.segmentedControlOutlet selectedSegmentIndex] == 0){
-        cell.imageView.image = [UIImage imageNamed:@"1"];
+        Pictures *pic = self.skyArray[indexPath.item];
+        cell.imageView.image = pic.image;
+        
         return cell;
     }
-     if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
-//        cell.imageView.image = self.imagesArray[indexPath.item];
-         cell.imageView.image = [UIImage imageNamed:@"sky1"];
+    if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
+        //        cell.imageView.image = self.imagesArray[indexPath.item];
+        Pictures *pic = self.otherArray[indexPath.item];
+        cell.imageView.image = pic.image;
         return cell;
     }
-    
-
-    
-    else { Pictures *pic = self.imagesArray[indexPath.section][indexPath.row];
-    cell.imageView.image = pic.image;
-    return cell;
+    else {
+        Pictures *pic = self.imagesArray[indexPath.section][indexPath.row];
+        cell.imageView.image = pic.image;
+        return cell;
     }
     // this doesn't work because it's not sending me a string back, its sending me a UIImage
     //    cell.imageView.image = [UIImage imageNamed:[self.imagesArray[indexPath.section] objectAtIndex:indexPath.row]];
@@ -113,30 +114,30 @@
 -(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     UICollectionReusableView *reusableView = nil;
     if (kind == UICollectionElementKindSectionHeader) {
-    HeaderCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
+        HeaderCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
         if ([self.segmentedControlOutlet selectedSegmentIndex] == 0) {
             headerView.headerLabel.text = [self.nameOfHeaders objectAtIndex:0];
         }
         if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
             headerView.headerLabel.text = [self.nameOfHeaders objectAtIndex:1];
         }else {
-        NSString *headerString = [self.nameOfHeaders objectAtIndex: indexPath.section];
-        NSString *title = headerString;
-        headerView.headerLabel.text = title;
+            NSString *headerString = [self.nameOfHeaders objectAtIndex: indexPath.section];
+            NSString *title = headerString;
+            headerView.headerLabel.text = title;
         }
         reusableView = headerView;
         
-
+        
     }
     return reusableView;
 }
 - (IBAction)controlPressed:(UISegmentedControl *)sender {
-   
+    
     if (sender.selectedSegmentIndex == 0) {
         
         [self.imagesCollection reloadData];
         
-    
+        
     } if (sender.selectedSegmentIndex == 1) {
         
         [self.imagesCollection reloadData];
@@ -144,8 +145,6 @@
     else {
         [self.imagesCollection reloadData];
     }
-    
-    
 }
 
 @end
