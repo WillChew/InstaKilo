16e9d6f348a244c7c393ca0431e394b7febd5310
WillChew <williamchew95@gmail.com>
2018-06-20 19:41:36 -0400

    can change screens but indexPath not working

--- InstaKilo/ViewController.m
+++ InstaKilo/ViewController.m
@@ -15,8 +15,8 @@
 @property (nonatomic,strong)NSArray<NSArray*> *imagesArray;
 @property (weak, nonatomic) IBOutlet UICollectionView *imagesCollection;
 @property (nonatomic, strong)NSArray *nameOfHeaders;
-@property (nonatomic,strong) NSArray *skyArray;
-@property (nonatomic,strong) NSArray *otherArray;
+@property (nonatomic,strong) NSArray<UIImage*> *skyArray;
+@property (nonatomic,strong) NSArray<UIImage*> *otherArray;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControlOutlet;
 
 @end
@@ -34,21 +34,20 @@
                           [[Pictures alloc]initWithImage:[UIImage imageNamed:@"sky2"] name:@"sky2"],
                           [[Pictures alloc]initWithImage:[UIImage imageNamed:@"sky3"] name:@"sky3"],
                           [[Pictures alloc]initWithImage:[UIImage imageNamed:@"sky4"] name:@"sky4"]];
+    
     self.skyArray = skyArray;
+    
     NSArray *everythingElseArray = @[[[Pictures alloc]initWithImage:[UIImage imageNamed:@"1"] name:@"1"],
     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"2"] name:@"2"],
     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"3"] name:@"3"],
     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"4"] name:@"4"],
     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"5"] name:@"5"],
     [[Pictures alloc]initWithImage:[UIImage imageNamed:@"6"] name:@"6"]];
+   
     self.otherArray = everythingElseArray;
     
     self.imagesArray = @[skyArray, everythingElseArray];
     self.nameOfHeaders = @[@"Animals", @"Everything Else"];
-//    NSArray *skyArray = @[[UIImage imageNamed:@"sky1"], [UIImage imageNamed:@"sky2"], [UIImage imageNamed:@"sky3"], [UIImage imageNamed:@"sky4"]];
-//    NSArray *everythingElse = @[[UIImage imageNamed:@"1"], [UIImage imageNamed:@"2"], [UIImage imageNamed:@"3"], [UIImage imageNamed:@"4"], [UIImage imageNamed:@"5"], [UIImage imageNamed:@"6"]];
-//    self.imagesArray = [NSArray arrayWithObjects:skyArray, everythingElse, nil];
-    NSLog(@"%@", self.imagesArray);
 }
 
 
@@ -58,45 +57,73 @@
 }
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    NSLog(@"%ld", self.imagesArray.count);
-    return [self.imagesArray count];
+
     
+    if ([self.segmentedControlOutlet selectedSegmentIndex]==0){
+        return 1;
+    }
+    if ([self.segmentedControlOutlet selectedSegmentIndex] == 1){
+        return 1;
+    } else {
+    return [self.imagesArray count];
+    }
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-//    if ([self.segmentedControlOutlet selectedSegmentIndex] == 0){
-        NSLog(@"WORKING");
-    NSLog(@"%ld", [[self.imagesArray objectAtIndex:section] count]);
-    return [[self.imagesArray objectAtIndex:section] count];
-    
-//    }
-    return [[self.imagesArray objectAtIndex:section] count];
+    if ([self.segmentedControlOutlet selectedSegmentIndex] == 0){
+        return [self.skyArray count];
 }
-
+    if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
+        return [self.otherArray count];
+    }
+    
+    else {
+        return [[self.imagesArray objectAtIndex:section] count];
+    }
+    }
+        
+        
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     ImageCellCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ImageCell" forIndexPath:indexPath];
+        
+        
+        
+//        cell.imageView.image = self.imagesArray[indexPath.section][indexPath.row];
+    if ([self.segmentedControlOutlet selectedSegmentIndex] == 0){
+        cell.imageView.image = [UIImage imageNamed:@"1"];
+        return cell;
+    }
+     if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
+//        cell.imageView.image = self.imagesArray[indexPath.item];
+         cell.imageView.image = [UIImage imageNamed:@"sky1"];
+        return cell;
+    }
     
-//important
-    Pictures *pic = self.imagesArray[indexPath.section][indexPath.row];
+
+    
+    else { Pictures *pic = self.imagesArray[indexPath.section][indexPath.row];
     cell.imageView.image = pic.image;
-
-    
-    //VVVV this doesn't work because it's not sending me a string back, its sending me a UIImage
-    //    cell.imageView.image = [UIImage imageNamed:[self.imagesArray[indexPath.section] objectAtIndex:indexPath.row]];
-
     return cell;
+    }
+    // this doesn't work because it's not sending me a string back, its sending me a UIImage
+    //    cell.imageView.image = [UIImage imageNamed:[self.imagesArray[indexPath.section] objectAtIndex:indexPath.row]];
 }
 
 -(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     UICollectionReusableView *reusableView = nil;
     if (kind == UICollectionElementKindSectionHeader) {
     HeaderCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView" forIndexPath:indexPath];
-       
+        if ([self.segmentedControlOutlet selectedSegmentIndex] == 0) {
+            headerView.headerLabel.text = [self.nameOfHeaders objectAtIndex:0];
+        }
+        if ([self.segmentedControlOutlet selectedSegmentIndex] == 1) {
+            headerView.headerLabel.text = [self.nameOfHeaders objectAtIndex:1];
+        }else {
         NSString *headerString = [self.nameOfHeaders objectAtIndex: indexPath.section];
-
         NSString *title = headerString;
         headerView.headerLabel.text = title;
+        }
         reusableView = headerView;
         
 
@@ -104,20 +131,21 @@
     return reusableView;
 }
 - (IBAction)controlPressed:(UISegmentedControl *)sender {
+   
     if (sender.selectedSegmentIndex == 0) {
         
         [self.imagesCollection reloadData];
-        NSLog(@"showing animals");
-    //filter array
-    } else {
-        NSLog(@"Showing everything else");
+        
+    
+    } if (sender.selectedSegmentIndex == 1) {
+        
+        [self.imagesCollection reloadData];
+    }
+    else {
         [self.imagesCollection reloadData];
     }
     
+    
 }
-
-
-
-
 
 @end
